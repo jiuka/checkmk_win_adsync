e46524fbf6f9a3e0260e4caf3a2f9049e0905ace
Marius Rieder <marius.rieder@durchmesser.ch>
2023-06-17 11:15:14 +0200

    Import updates from template

--- .devcontainer/build.sh
+++ .devcontainer/build.sh
@@ -1,15 +1,20 @@
 #!/bin/bash
 
-NAME=$(python -c 'print(eval(open("package").read())["name"])')
-rm /omd/sites/cmk/var/check_mk/packages/* ||:
-ln -s $WORKSPACE/package /omd/sites/cmk/var/check_mk/packages/$NAME
+NAME=$(python3 -c 'print(eval(open("package").read())["name"])')
+VERSION=$(python3 -c 'print(eval(open("package").read())["version"])')
+rm -f $NAME-$VERSION.mkp \
+   /omd/sites/cmk/var/cat  check_mk/packages/${NAME}-*.mkp \
+   /omd/sites/cmk/var/check_mk/packages_local/${NAME}-*.mkp ||:
 
-mkp -v pack $NAME
+mkp -v package package 2>&1 | sed '/Installing$/Q' ||:
+
+cp /omd/sites/cmk/var/check_mk/packages_local/$NAME-$VERSION.mkp .
+
+mkp inspect $NAME-$VERSION.mkp
 
 # Set Outputs for GitHub Workflow steps
 if [ -n "$GITHUB_WORKSPACE" ]; then
-    echo "::set-output name=pkgfile::$(ls *.mkp)"
-    echo "::set-output name=pkgname::${NAME}"
-    VERSION=$(python -c 'print(eval(open("package").read())["version"])')
-    echo "::set-output name=pkgversion::$VERSION"
+    echo "pkgfile=${NAME}-${VERSION}.mkp" >> $GITHUB_OUTPUT
+    echo "pkgname=${NAME}" >> $GITHUB_OUTPUT
+    echo "pkgversion=$VERSION" >> $GITHUB_OUTPUT
 fi
